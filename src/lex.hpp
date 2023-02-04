5b14fb6a8bd28f317df5a4f10bbe5b86a34fe3d2
Ahmer Raza <ahhmerrr.dev@gmail.com>
2023-02-04 10:52:28 -0500

    Lexer template
    Created template for lexer

---
+++ src/lex.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#ifndef LEX_HPP
+#define LEX_HPP
+/**
+ * @brief parses the input file
+ *
+ * @param infile file to parse
+ *
+ * Parses file line-by-line and splits it into tokens
+ */
+void parse(ifstream infile);
+
+#endif
---
+++ syntax.md
@@ -0,0 +1,1 @@
+This is where the designed syntax goes.
